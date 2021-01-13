de66d4bb643fac7e1831a8096c9e222af69ce38f
Ismael Baena <ismael.baena.7e3@itb.cat>
2021-01-13 12:27:15 +0100

    Menu equipable v2
    -Los slots de cada parte del cuerpo cambian de color según si el item se puede colocar en ese lugar o no.
    -Se ha solucionado el error de los tamaños del menu cuando la resolucion de la pantalla se cambia.
    -El menu solo se muestra cuando el jugador entra dentro del collider del item equipable.

--- Assets/Scripts/ItemMenu/ShowMenu.cs
+++ Assets/Scripts/ItemMenu/ShowMenu.cs
@@ -2,31 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowMenu : MonoBehaviour
-{
+public class ShowMenu : MonoBehaviour {
 
-    public GameObject itemMenu;
-    private void Start()
-    {
-        Debug.Log("El objeto vive");
-    }
+    public GameObject itemMenu, cabeza, torso, brazoDerecho, brazoIzquierdo, piernaDerecha, piernaIzquierda;
 
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter2D(Collider2D collision) {
         Debug.Log("Ha entrado");
-        if (other.gameObject.tag == "Player")
-        {
+        if (collision.gameObject.tag == "Player") {
+            string tag = this.gameObject.tag;
             itemMenu.SetActive(true);
+            switch(tag) {
+                case "Cabeza":
+                    cambiarColor(cabeza);
+                    break;
+                case "Torso":
+                    cambiarColor(torso);
+                    break;
+                case "Brazos":
+                    cambiarColor(brazoDerecho);
+                    cambiarColor(brazoIzquierdo);
+                    break;
+                case "Piernas":
+                    cambiarColor(piernaDerecha);
+                    cambiarColor(piernaIzquierda);
+                    break;
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit2D(Collider2D collision) {
         Debug.Log("Ha salido");
-        if (other.gameObject.tag == "Player")
-        {
+        if (collision.gameObject.tag == "Player") {
             itemMenu.SetActive(false);
         }
     }
-    
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        string tag = this.gameObject.tag;
+        switch (tag) {
+            case "Cabeza":
+                if (Input.GetButtonDown("EquiparCabeza")) Debug.Log("Se ha equipado la cabeza");
+                break;
+            case "Torso":
+                if (Input.GetButtonDown("EquiparTorso")) Debug.Log("Se ha equipado el torso");
+                break;
+            case "Brazos":
+                if (Input.GetButtonDown("EquiparBrazoDerecho")) Debug.Log("Se ha equipado el brazo derecho");
+                if (Input.GetButtonDown("EquiparBrazoIzquierdo")) Debug.Log("Se ha equipado el brazo izquierdo");
+                break;
+            case "Piernas":
+                if (Input.GetButtonDown("EquiparPiernaDerecha")) Debug.Log("Se ha equipado la pierna derecha");
+                if (Input.GetButtonDown("EquiparPiernaIzquierda")) Debug.Log("Se ha equipado la pierna izquierda");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void cambiarColor(GameObject slot) {
+        CanvasRenderer renderer = slot.GetComponent<CanvasRenderer>();
+        renderer.SetColor(Color.green);
+    }
 }
