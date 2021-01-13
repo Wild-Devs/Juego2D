5e953e985dfe78a9767f9b3c0363db3b7ab106f7
IsmaelBaena <73363907+IsmaelBaena@users.noreply.github.com>
2021-01-13 17:27:33 +0100

    Trabajo de clase

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -10,6 +10,7 @@ public class PlayerStats : MonoBehaviour
     public float attack;
     public float speed;
     public int def;
+    public GameObject cabeza, torso, brazoIzquierdo, brazoDerecho, piernaIzquierda, piernaDerecha;
     
     
     //HEALTH//
@@ -76,4 +77,45 @@ public class PlayerStats : MonoBehaviour
         return speed;
 
     }
+
+    //EQUIPO CABEZA//
+    public void setCabeza(GameObject equipable) {
+        if (cabeza == null) {
+
+        }
+        cabeza = equipable;
+    }
+
+    public GameObject getCabeza() {return cabeza;}
+
+    //EQUIPO TORSO//
+    public void setTorso(GameObject equipable) {torso = equipable; }
+
+    public GameObject getTorso() {
+        if (torso == null) return null;
+        else return torso;
+    }
+
+    //EQUIPO BRAZO DERECHO//
+    public void setBrazoDerecho(GameObject equipable) {brazoDerecho = equipable; }
+
+    public GameObject getBrazoDerecho() { return brazoDerecho; }
+
+    //EQUIPO BRAZO IZQUIERDO//
+    public void setBrazoIzquierdo(GameObject equipable) {brazoIzquierdo = equipable; }
+
+    public GameObject getBrazoIzquierdo() { return brazoIzquierdo;}
+
+
+    //EQUIPO PIERNA IZQUIERDA//
+    public void setPiernaIzquierda(GameObject equipable) { piernaIzquierda = equipable; }
+
+    public GameObject getPiernaIzquierda() { return piernaIzquierda; }
+
+    //EQUIPO PIERNA DERECHA//
+    public void setPiernaDerecha(GameObject equipable) { piernaDerecha = equipable; }
+
+    public GameObject getPiernaDerecha() { return piernaDerecha; }
+
+
 }
